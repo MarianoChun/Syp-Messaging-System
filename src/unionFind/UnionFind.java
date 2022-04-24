d06637854e5de734b3b510a7cbc56b54cef555d8
Mariano Chun <marianochun01@gmail.com>
2022-04-24 19:48:33 -0300

    Add clase UnionFind

---
+++ src/unionFind/UnionFind.java
@@ -0,0 +1,40 @@
+package unionFind;
+
+import java.util.ArrayList;
+
+import grafos.GrafoNDPonderado;
+
+public class UnionFind {
+	private ArrayList<Integer> vertices;
+	
+	public UnionFind(GrafoNDPonderado g) {
+		this.vertices = new ArrayList<Integer>();
+		this.vertices.addAll(g.conjuntoDeVertices());
+	}
+	
+	// Me devuelve la raiz de la componente conexa del vertice i
+	public int root(int i) {
+		while(vertices.get(i) != i) {
+			i = vertices.get(i);
+		}
+		
+		return i;
+	}
+	
+	// Determina si dos vertices estan en la misma componente conexa
+	public boolean find(int i, int j) {
+		return root(i) == root(j);
+	}
+	
+	// Hace que la raiz de una componente conexa apunte a la otra (union)
+	public void union(int i, int j) {
+		int raizI = root(i);
+		int raizJ = root(j);
+		
+		vertices.set(raizI, raizJ);
+	}
+	
+	public static void main(String[] args) {
+
+	}
+}
