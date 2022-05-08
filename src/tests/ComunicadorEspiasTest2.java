f95e241a5295383ba9381091280898d0aff68e85
Mariano Chun <marianochun01@gmail.com>
2022-05-08 16:42:08 -0300

    Add casos de test

--- src/tests/ComunicadorEspiasTest2.java
+++ src/tests/ComunicadorEspiasTest2.java
@@ -2,6 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -59,6 +62,25 @@ public class ComunicadorEspiasTest2 {
 	}
 
 	@Test
+	public void agmPrimTest() {
+		GrafoNDPEtiquetado agmEsperado = new GrafoNDPEtiquetado(c.cantidadEspias());
+
+		agmEsperado.agregarArista(new Vertice(0, "juan"), new Vertice(1, "ivan"), 0.1); // Juan, Ivan, 0.1
+		agmEsperado.agregarArista(new Vertice(2, "RuBen"), new Vertice(11, "HUGO"), 0.4); // Ruben, Hugo, 0.4
+		agmEsperado.agregarArista(new Vertice(3, "William"), new Vertice(5, "PePe"), 0.3); // William, Pepe, 0.3
+		agmEsperado.agregarArista(new Vertice(4, "RoDOLFo"), new Vertice(0, "Juan"), 0.4); // Rodolfo, Juan, 0.4
+		agmEsperado.agregarArista(new Vertice(6, "JOSE"), new Vertice(7, "mabel"), 0.9); // Jose, Mabel, 0.9
+		agmEsperado.agregarArista(new Vertice(8, "AlVaro"), new Vertice(5, "pepe"), 0.5); // Alvaro, Pepe, 0.5
+		agmEsperado.agregarArista(new Vertice(9, "Julieta"), new Vertice(1, "IVAn"), 0.1); // Julieta, Ivan, 0.1
+		agmEsperado.agregarArista(new Vertice(7, "MaBeL"), new Vertice(12, "CandeLA"), 0.8); // Mabel, Candela, 0.8
+		agmEsperado.agregarArista(new Vertice(8, "alvaro"), new Vertice(10, "Gabriela"), 0.7); // Alvaro, Gabriela, 0.7
+		agmEsperado.agregarArista(new Vertice(0, "juan"), new Vertice(2, "ruben"), 0.9); // Juan, Ruben, 0.9
+		agmEsperado.agregarArista(new Vertice(2, "rUBEN"), new Vertice(3, "william"), 0.7); // Ruben, William, 0.7
+		agmEsperado.agregarArista(new Vertice(7, "mabel"), new Vertice(10, "gabriela"), 0.9); // Mabel, Gabriela, 0.9
+		
+		assertEquals(agmEsperado, c.obtenerRedSeguraPrim());
+	}
+	@Test
 	public void agregarEspiasMayusculasTest() {
 		assertTrue(c.existeComunicacion("JuLieTa", "IVAN") && c.existeComunicacion("ruben", "HuGO"));
 	}
@@ -68,7 +90,33 @@ public class ComunicadorEspiasTest2 {
 		double esperado = 0.1;
 		assertTrue(esperado == c.obtenerProbabIntercepcion("juan", "ivan"));
 	}
+	
+	@Test
+	public void obtenerIndiceEspiaTest() {
+		double esperado = 3;
+		assertTrue(esperado == c.obtenerIndiceEspia("William"));
+	}
 
+	@Test
+	public void obtenerEspiasTest() {
+		Map<String, Integer> espiasEsperados = new HashMap<String, Integer>();
+		espiasEsperados.put("juan", 0);
+		espiasEsperados.put("ivan", 1);
+		espiasEsperados.put("ruben", 2);
+		espiasEsperados.put("william", 3);
+		espiasEsperados.put("rodolfo", 4);
+		espiasEsperados.put("pepe", 5);
+		espiasEsperados.put("jose", 6);
+		espiasEsperados.put("mabel", 7);
+		espiasEsperados.put("alvaro", 8);
+		espiasEsperados.put("julieta", 9);
+		espiasEsperados.put("gabriela", 10);
+		espiasEsperados.put("hugo", 11);
+		espiasEsperados.put("candela", 12);
+		
+		assertTrue(espiasEsperados.equals(c.obtenerEspias()));
+	}
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void verificarExistenEspiasTest() {
 		c.agregarComunicacion("Mariano", "Tito", 0.2);
